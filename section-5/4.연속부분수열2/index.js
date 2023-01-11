2c6f77b7348a65d171d13e259bc9763669879228
wooram <zzzwwwrrr66@gmail.com>
2023-01-12 02:13:38 +0900

    <add> multipoint

---
+++ section-5/4.연속부분수열2/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(path).toString().split("\n").map(v=>v.split(" ").map(v=>v*1));
+const [[n, m], [...nums]] = input;
+let sum = 0;
+let cnt = 0;
+let lt = 0;
+// rt - lt + 1 === 현재까지의 value 들의 개수 세기
+for(let rt = 0; rt < n; rt++) {
+  console.log(rt - lt + 1);
+}
