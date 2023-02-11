190d738d34a0f77a8f7276a53804be038c33bf66
wooram <zzzwwwrrr66@gmail.com>
2023-02-12 02:46:03 +0900

    <add> BFS algoritme

---
+++ section-9/5.송아지 찾기(BFS : 상태트리탐색)/solve.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [start, target] = 
+fs
+.readFileSync(path)
+.toString()
+.split(" ")
+.map(v=>v*1);
+
+/* 
+현수는 송아지를 잃어버렸다. 다행히 송아지에는 위치추적기가 달려 있다. 
+현수의 위치와 송아 지의 위치가 수직선상의 좌표 점으로 주어지면 
+현수는 현재 위치에서 송아지의 위치까지 다음 과 같은 방법으로 이동한다.송아지는 움직이지 않고 제자리에 있다.
+현수는 스카이 콩콩을 타고 가는데 한 번의 점프로 앞으로 1, 뒤로 1, 앞으로 5를 이동할 수 있다. 
+최소 몇 번의 점프로 현수가 송아지의 위치까지 갈 수 있는지 구하는 프로그램을 작성 하세요.
+*/
+// nv = [1, -1, 5]
+
+function solve(start, target, Q = []) {
+  const check = Array.from({length: 10001}, ()=>0);
+  function BFS(L, start) {
+    if(check[start] === 1) return L;
+    if(start === target) {
+      return L;
+    } else {
+      for(let nv of [1, -1, 5]) {
+        check[start] = 1;
+        BFS(L + 1, start + nv);
+      }
+    }
+  }
+  BFS(0, start)
+}
+console.log(
+  solve(start, target)
+);
